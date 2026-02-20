04bd981c8529ed6336252fdbd19c68a74752c92c
Harsh Ambastha <harshambastha0204@gmail.com>
2026-02-20 19:36:53 +0530

    Refactored TicTacToe draw logic code

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -3,37 +3,46 @@ import styles from './TicTacToe.module.css';
 
 const TicTacToe = () => {
     const [grid, setGrid] = useState(getInitialGrid());
-    const [playerTurn, setPlayerTurn] = useState("p1");
-    let [GAME_ENDED, GAME_DRAW] = isGameEnded(grid);
+    const [isFirstPlayerTurn, setIsFirstPlayerTurn] = useState(true);
+    const status = getGameStatus(grid);
 
-    console.log(`GAME_ENDED=${GAME_ENDED}`);
 
     const handleGridClick = (event) => {
-        const index = event.target.dataset.index;
-        if (GAME_ENDED || grid[index]) return;
+        const index = +event.target.dataset.index;
+        if (status || grid[index]) return;
+
         setGrid(prev => {
-            prev[index] = playerTurn == 'p1' ? 'X' : 'O';
-            return prev;
+            return prev.map((_, idx) => {
+                if (index === idx) {
+                    return isFirstPlayerTurn ? 'X' : 'O';
+                }
+                return prev[idx];
+            });
         });
-        setPlayerTurn(prev => {
-            if (prev == 'p1') return 'p2';
-            return 'p1';
-        });
+        setIsFirstPlayerTurn(prev => !prev);
     }
 
     const onReset = () => {
         setGrid(getInitialGrid());
-        setPlayerTurn('p1');
-        GAME_ENDED = false;
+        setIsFirstPlayerTurn(true);
+    }
+
+    const getStatusMessage = () => {
+        if (!status) {
+            return isFirstPlayerTurn ? "Player X Turn" : "Player O Turn";
+        }
+        if (status === "DRAW") {
+            return "Game Draw";
+        }
+        return `Player ${status} Wins 🎉`;
     }
 
     return (
         <div className={styles['game-container']}>
-            {GAME_ENDED ? GAME_DRAW ? "Game Draw" : playerTurn == 'p1' ? "Player O Wins" : "Player X Wins" : ''}
-            {!GAME_ENDED ? playerTurn == "p1" ? "Player X Turn" : "Player O Turn" : ""}
+            <span className={styles['status-message']}>{getStatusMessage()}</span>
             <div className={styles['grid-container']} onClick={handleGridClick}>
                 {grid.map((space, index) => (
-                    <div key={`key-${index}`} data-index={index} className={styles.grid}>{space == null ? "" : space}</div>
+                    <div key={`key-${index}`} data-index={index} className={styles.cell}>{space == null ? "" : space}</div>
                 ))}
             </div>
             <button className={styles['reset-button']} onClick={onReset}>Reset</button>
@@ -45,31 +54,27 @@ function getInitialGrid() {
     return Array.from({ length: 9 }).fill(null);
 }
 
-function isGameEnded(grid) {
+function getGameStatus(grid) {
     const a = grid[0] && grid[0] == grid[1] && grid[0] == grid[2];
-    if (a) return [true, false];
+    if (a) return grid[0];
     const b = grid[0] && grid[0] == grid[3] && grid[0] == grid[6];
-    if (b) return [true, false];
+    if (b) return grid[0];
     const c = grid[0] && grid[0] == grid[4] && grid[0] == grid[8];
-    if (c) return [true, false];
+    if (c) return grid[0];
     const d = grid[1] && grid[1] == grid[4] && grid[1] == grid[7];
-    if (d) return [true, false];
+    if (d) return grid[1];
     const e = grid[3] && grid[3] == grid[4] && grid[3] == grid[5];
-    if (e) return [true, false];
+    if (e) return grid[3];
     const f = grid[6] && grid[6] == grid[7] && grid[6] == grid[8];
-    if (f) return [true, false];
+    if (f) return grid[6];
     const g = grid[2] && grid[2] == grid[5] && grid[2] == grid[8];
-    if (g) return [true, false];
+    if (g) return grid[2];
     const h = grid[2] && grid[2] == grid[4] && grid[2] == grid[6];
-    if (h) return [true, false];
+    if (h) return grid[2];
 
-    const index = grid.findIndex(item => item === null);
-    console.log(`index=${index}`);
-    if (index == -1) {
-        return [true, true];
-    }
-
-    return [false, false];
+    const isDraw = grid.every(space => space != null);
+    if (isDraw) return "DRAW";
+    return null;
 }
 
 export default TicTacToe;
